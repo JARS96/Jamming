a89f7ee9dc8a18e53122bb5e60f70c432de8749c
JARS <J.ARS96@outlook.com>
2022-07-12 14:41:12 -0500

    app & playlist js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName=this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -45,8 +46,9 @@ class App extends React.Component {
     this.setState({playlistName: name});
   }
 
-  savePlaylist(){
-    
+  savePlaylist() {
+    alert('this method is linked to the button correctly') 
+    const trackUris = this.state.playlistTracks.map(track => track.uri);    
   }
 
   render() {
@@ -57,7 +59,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} /> 
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} /> 
           </div>
         </div>
       </div>
